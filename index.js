7daedb86c88bd2d79a48821e3c245fad44ccbaf4
Vladimir D <ande.vladimir@gmail.com>
2018-02-18 22:20:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-function theBeatlesPlay(musician, instruments){
+function theBeatlesPlay (musicians, instruments){
   var array = []; 
-  for (var i=0; i<musician.length; i++){
-    array.push(musician[i] + " plays "+ instruments[i]);
+  for (let i=0; i<musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array;
 }
 function johnLennonFacts(facts){
   var array = [];
-  let i=0; 
-  while(i<facts.length){
-    array.push(facts[i]+"!!!");
-    i++;
+  for (let i=0; i<facts.length; i++){
+    array.push(`${facts[i]}!!!`);
   }
   return array;
 }
-function iLoveTheBeatles(n){
+function iLoveTheBeatles(number){
   var array = [];
-  do{
-    array.push("I love the Beatles!");
-    n++;
-  }while(n<15); 
+  do { array.push('I love the Beatles!')
+    number++; 
+  }while(number<15);
   return array;
 }
